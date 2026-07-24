0d76814607d8ce60974111c562836003ae75f169
Habhi35 <yellapuhabhiram@gmail.com>
2026-07-24 18:41:34 +0530

    new changes on the products

--- products/models.py
+++ products/models.py
@@ -3,3 +3,14 @@ from django.db import models
 # Create your models here.
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
